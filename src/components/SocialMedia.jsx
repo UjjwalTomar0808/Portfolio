14ec69817377a603e53b1eea8d653b929d6e3364
ujjwal <ujjwaltomar.0808@gmail.com>
2024-09-12 00:04:15 +0530

    update some icons and links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.instagram.com/ujjwaltomar0808" target="_blank" rel="noopener noreferrer">
+        <BsInstagram />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://www.linkedin.com/in/ujjwal-tomar-26a455288" target="_blank" rel="noopener noreferrer">
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://github.com/UjjwalTomar0808" target="_blank" rel="noopener noreferrer">
+        <BsGithub />
+      </a>
     </div>
   </div>
 );
